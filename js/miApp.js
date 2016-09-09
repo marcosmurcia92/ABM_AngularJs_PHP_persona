01ccc56d3357e958178287049cf7c025863ccd88
marcosmurcia92 <marcosmurcia92@gmail.com>
2016-09-08 21:14:44 -0300

    Clase de Ruteo Cambios de ultima hora. Estados de Alta, Menu y Grilla agregados.

--- js/miApp.js
+++ js/miApp.js
@@ -42,7 +42,19 @@ miApp.config(function($stateProvider,$urlRouterProvider){
 				}
 			}
 		)
-	$urlRouterProvider.otherwise("/persona/alta");
+		.state(
+			"persona.grilla",
+			{
+				url:'/grilla',
+				views:{
+					"contenido":{
+						templateUrl:'personaGrilla.html',
+						controller:'controlPersonaGrilla'
+					}
+				}
+			}
+		)
+	$urlRouterProvider.otherwise("/persona/grilla");
 });
 
 miApp.controller("controlInicio",function($scope){
@@ -54,5 +66,62 @@ miApp.controller("controlPersonaMenu",function($scope){
 });
 
 miApp.controller("controlPersonaAlta",function($scope){
+  $scope.DatoTest="**alta**";
 
+//inicio las variables
+  $scope.persona={};
+  $scope.persona.nombre= "natalia" ;
+ $scope.persona.dni= "12312312" ;
+  $scope.persona.apellido= "natalia" ;
+  $scope.persona.foto="sinfoto";
+  
+
+
+  $scope.Guardar=function(){
+
+
+  	console.log("persona a guardar:");
+    console.log($scope.persona);
+
+    
+    $http.post('PHP/nexo.php', { datos: {accion :"insertar",persona:$scope.persona}})
+ 	  .then(function(respuesta) {     	
+ 		     //aca se ejetuca si retorno sin errores      	
+      	 console.log(respuesta.data);
+
+    },function errorCallback(response) {     		
+     		//aca se ejecuta cuando hay errores
+     		console.log( response);     			
+ 	  });
+
+  
+
+  }
+});
+
+miApp.controller("controlPersonaGrilla",function($scope,$http){
+  	$scope.DatoTest="**grilla**";
+ 	  console.log("Estoy en la Grilla");
+
+    $scope.ListadoPersonas = [];
+
+ 	$http.get('http://www.mocky.io/v2/57c8ab91120000e613e76a88')
+  .then(function(respuesta){
+    //CORRECTO
+    $scope.ListadoPersonas = respuesta.data;
+    console.info("Volvio",respuesta.data);
+  },function(error){
+    //INCORRECTO
+    $scope.ListadoPersonas = [];
+    console.info("ERROR",error);
+  });
+
+ 	$scope.Borrar=function(persona){
+		console.log("borrar"+persona);
+	}
+
+ 	$scope.Modificar=function(id){
+ 		
+ 		console.log("Modificar"+id);
+ 	}
 });
